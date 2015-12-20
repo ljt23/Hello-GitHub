f9394d470a141a0195d5ea22d73cc07829b428e0
ljt23 <ljt23@126.com>
2015-12-20 20:02:58 +0800

    create project Hello-GitHub

---
+++ Hello-GitHub/Hello-GitHub/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  Hello-GitHub
+//
+//  Created by 罗俊涛 on 15/12/20.
+//  Copyright © 2015年 罗俊涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ Hello-GitHub/Hello-GitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hello-GitHub
+//
+//  Created by 罗俊涛 on 15/12/20.
+//  Copyright © 2015年 罗俊涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
