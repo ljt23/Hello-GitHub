25b640008868615e22a336b3dd8d17a767c26d83
ljt23 <ljt23@126.com>
2015-12-20 21:35:03 +0800

    create project Hello-GitHub-Tabbed

---
+++ Hello-GitHub-Tabbed/Hello-GitHub-Tabbed/SecondViewController.m
@@ -0,0 +1,27 @@
+//
+//  SecondViewController.m
+//  Hello-GitHub-Tabbed
+//
+//  Created by 罗俊涛 on 15/12/20.
+//  Copyright © 2015年 罗俊涛. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
